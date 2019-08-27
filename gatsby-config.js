6b97c407cd2bfc909f3ea1b368e228b225221a4f
Irwin Razaghi <irwin.razaghi@oohmedia.com.au>
2019-08-27 18:29:47 +1000

    Removed unused Gatsby plugins.

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,46 +5,34 @@ module.exports = {
     author: "Irwin Razaghi <irwin@razaghi.com.au>",
   },
   plugins: [
-    // "gatsby-plugin-eslint",
+    "gatsby-plugin-eslint",
     "gatsby-plugin-transition-link",
     {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      resolve: "gatsby-plugin-prefetch-google-fonts",
       options: {
         fonts: [
           {
-            family: `Karla`,
+            family: "Karla",
           },
           {
-            family: `Source Sans Pro`,
+            family: "Source Sans Pro",
           },
         ],
       },
     },
     "gatsby-plugin-react-helmet",
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: "gatsby-plugin-manifest",
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: "razaghi.com.au",
+        short_name: "razaghi.com.au",
+        start_url: "/",
+        background_color: "#663399",
+        theme_color: "#663399",
+        display: "minimal-ui",
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `razaghi.com.au`,
-        short_name: `razaghi.com.au`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    `gatsby-plugin-styled-components`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-offline",
   ],
 };
